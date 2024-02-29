dd3090bbf424b05a2162101e735429d96c1040f9
rithy <rithyyano.hoy@gmail.com>
2024-02-29 23:22:49 +0100

    correction

--- srcs/parsing/condition_launch.c
+++ srcs/parsing/condition_launch.c
@@ -6,23 +6,23 @@
 /*   By: rihoy <rihoy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 14:05:03 by rihoy             #+#    #+#             */
-/*   Updated: 2024/02/29 23:10:54 by rihoy            ###   ########.fr       */
+/*   Updated: 2024/02/29 23:21:35 by rihoy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-#include "../../utils/lib_utils.h"
+#include "lib_utils.h"
 
 void	launch_shell(int argc, const char **env)
 {
 	if (argc != 1)
 	{
-		print_error("MINISHELL >: 'argument not require'");
+		print_error("MINISHELL >: 'argument not require'\n");
 		exit(0);
 	}
 	if (!env | !env[0])
 	{
-		print_error("MINISHELL >: 'environement require'");
+		print_error("MINISHELL >: 'environement require'\n");
 		exit(0);
 	}
 }
