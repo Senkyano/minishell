f7281b146725045335b246c303cb02364656db3d
rithy <rithyyano.hoy@gmail.com>
2024-03-03 22:58:30 +0100

    update

--- srcs/parsing/get_process.c
+++ srcs/parsing/get_process.c
@@ -6,7 +6,7 @@
 /*   By: rihoy <rihoy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/01 14:36:59 by rihoy             #+#    #+#             */
-/*   Updated: 2024/03/03 13:42:17 by rihoy            ###   ########.fr       */
+/*   Updated: 2024/03/03 19:29:13 by rihoy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,16 @@
 
 void	get_all_process(char *process, t_shell *bash)
 {
+	int	i;
+
+	i = -1;
 	bash->space = lib_split(process, " ");
 	if (!bash->space)
 		gestion_exit("Malloc fail\n", bash);
-	// Premiere choses a faire c'est de faire un un split pour tout les espace.
+	while (bash->space[++i])
+	{
+	// analyse du space.
 	// Dans le get process on auras d'abord le get_all_cmd.
 		// Je vais devoir faire un split entre pipe
+	}
 }
