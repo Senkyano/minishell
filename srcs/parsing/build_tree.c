b4d30383cd8e3178e8fd0c5847759095428126cb
Senkyano <rithyyano.hoy@gmail.com>
2024-04-01 21:57:03 +0200

    think how to build tree

---
+++ srcs/parsing/build_tree.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   build_tree.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rihoy <rihoy@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/01 19:29:42 by rihoy             #+#    #+#             */
+/*   Updated: 2024/04/01 21:54:39 by rihoy            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+t_tree	*build_tree(t_infopars *lst_char, int parenthese, t_tree *main_tree)
+{
+	t_infopars	*curr;
+	t_tree		*curr_branch;
+
+	if (curr->spe == 1)
+		// faire les branches pour les && ou  ||
+	else if (curr->spe == 2)
+		// faire le bloc de gauche du dernier.
+	// faire le bloc de droit.
+	return (curr_branch);
+}
